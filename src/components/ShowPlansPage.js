9a3759e404d50d5af90769f37d1de0058e070e91
Ash1win <naikashwin30@gmail.com>
2022-10-17 21:56:59 +0530

    search suggestions and autocomplete

--- src/components/ShowPlansPage.js
+++ src/components/ShowPlansPage.js
@@ -5,6 +5,7 @@ import doUpdate from "./UpdatePage";
 import Trash from "../icons/icons";
 import { toast } from "react-toastify";
 import DeleteConfirmation from "./DeleteConfirmation";
+import { list } from "postcss";
 
 
 export default function ShowPlansPage() {
@@ -33,6 +34,10 @@ export default function ShowPlansPage() {
     const [ee, setEe] = useState(5) //end index
     const [iVal, setIVal] = useState(5)
 
+    //auto-complete-search
+    const [showAutoComp, setShowAutoComp] = useState(true)
+    const [autoSearchList, setAutoSearchList] = useState([])
+
     useMemo(() => {
         setSCurrsrch(sAllsrch.slice(si, ee))
         console.log(si+' '+ee)
@@ -42,6 +47,13 @@ export default function ShowPlansPage() {
 
     // search function by planNo or name or description
     function search() {
+
+        //hide auto search
+            let list = []
+            setAutoSearchList(list)
+            setShowAutoComp(false)
+
+        console.log("search called!! s:"+searchText)
         if(searchText === '') {
             setIsSearching(false)
         }else {
@@ -83,6 +95,66 @@ export default function ShowPlansPage() {
                 }
 
                 let searchResult = AllMobilePlans.filter((plan) => plan.validity == searchText)
+                console.log(searchResult)
+                setSAllsrch(searchResult)
+                setSi(0)
+                setEe(5)
+                setSCurrsrch(searchResult.slice(si, ee))
+            }
+        }
+    }
+
+
+    //search function for autocomplete
+    function search2(stxt) {
+
+        //hide auto search
+            let list = []
+            setAutoSearchList(list)
+            setShowAutoComp(false)
+
+        console.log("search called!! s:"+stxt)
+        if(stxt === '') {
+            setIsSearching(false)
+        }else {
+            setIsSearching(true)
+            if(srchType === 'planno'){
+                // let searchResult = AllMobilePlans.find((plan) => plan.id == stxt)
+                // let searchResults = []
+                // searchResults.push(searchResult)
+                // console.log(searchResults)
+                // setSCurrsrch(searchResults)
+
+                if(isNaN(stxt)) {
+                    toast.error("plan no. should be a number")
+                }
+
+                let searchResult = AllMobilePlans.filter((plan) => plan.id == stxt)
+                console.log(searchResult)
+                setSAllsrch(searchResult)
+                setSi(0)
+                setEe(5)
+                setSCurrsrch(searchResult.slice(si, ee))
+            }else if(srchType === 'name') {
+                let searchResult = AllMobilePlans.filter((plan) => plan.name.toLowerCase().includes(stxt.toLowerCase()))
+                console.log(searchResult)
+                setSAllsrch(searchResult)
+                setSi(0)
+                setEe(5)
+                setSCurrsrch(searchResult.slice(si, ee))
+            }else if(srchType === 'description') {
+                let searchResult = AllMobilePlans.filter((plan) => plan.description.toLowerCase().includes(stxt.toLowerCase()))
+                console.log(searchResult)
+                setSAllsrch(searchResult)
+                setSi(0)
+                setEe(5)
+                setSCurrsrch(searchResult.slice(si, ee))
+            }else if(srchType === 'validity'){
+                if(isNaN(stxt)) {
+                    toast.error("Validity should be a number")
+                }
+
+                let searchResult = AllMobilePlans.filter((plan) => plan.validity == stxt)
                 console.log(searchResult)
                 setSAllsrch(searchResult)
                 setSi(0)
@@ -163,6 +235,9 @@ export default function ShowPlansPage() {
 
 
     useEffect(() => {
+            let list = []
+            setAutoSearchList(list)
+            setShowAutoComp(false)
         getAllPlans().then((res) => {
             setAllMobilePlans(res.reverse());
             setMobilePlans(res.slice(startIndex, endIndex))
@@ -171,6 +246,9 @@ export default function ShowPlansPage() {
     }, [])
 
     useMemo(() => {
+            let list = []
+            setAutoSearchList(list)
+            setShowAutoComp(false)
 
         setMobilePlans(AllMobilePlans.slice(startIndex, endIndex))
 
@@ -196,7 +274,68 @@ export default function ShowPlansPage() {
 
     function handleSearch(e) {
         setSearchText(e.target.value)
+        if(srchType === "planno" && e.target.value != '') {
+            setShowAutoComp(true)
+            let list = []
+            AllMobilePlans.map((plan) => {
+                if((plan.id+'').includes(e.target.value)) {
+                    list.push(plan.id)
+                }
+            })
+            setAutoSearchList(list)
+        }else if(srchType === "name" && e.target.value != '') {
+            setShowAutoComp(true)
+            let list = []
+            AllMobilePlans.map((plan) => {
+                if(plan.name.toLowerCase().includes((''+e.target.value).toLocaleLowerCase())) {
+                    if(list.indexOf(plan.name) < 0){
+                        list.push(plan.name)
+                    }
+                }
+            })
+            setAutoSearchList(list)
+        }else if(srchType === "description" && e.target.value != '') {
+            setShowAutoComp(true)
+            let list = []
+            AllMobilePlans.map((plan) => {
+                if(plan.description.toLowerCase().includes((''+e.target.value).toLocaleLowerCase())) {
+                    if(!list.includes(plan.description)) {
+                        list.push(plan.description)
+                    }
+                }
+            })
+            console.log(list)
+            setAutoSearchList(list)
+        }else if(srchType === "validity" && e.target.value != '') {
+            setShowAutoComp(true)
+            let list = []
+            AllMobilePlans.map((plan) => {
+                if((plan.validity+'').includes(e.target.value)) {
+                    if(list.indexOf(plan.validity) < 0){
+                        list.push(plan.validity)
+                    }
+                }
+            })
+
+            setAutoSearchList(list)
+        }
         // console.log(e.target.value)
+
+        if(e.target.value === '') {
+            let list = []
+            setAutoSearchList(list)
+            setShowAutoComp(false)
+        }
+    }
+
+    function handleAutoSearch(searchtxt) {
+        console.log(searchtxt)
+        setSearchText(searchtxt)
+        setShowAutoComp(false)
+
+
+        search2(searchtxt)
+
     }
 
     return (
@@ -218,28 +357,36 @@ export default function ShowPlansPage() {
                 <div class="flex">
                     {/* <label for="search-dropdown" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">Your Email</label> */}
                     {/* <button id="dropdown-button" data-dropdown-toggle="dropdown" class="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600" type="button">All categories <svg aria-hidden="true" class="ml-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></button> */}
-                    <select onChange={handleSearchSelect} className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100" aria-labelledby="dropdown-button">
+                    <select onChange={handleSearchSelect} className="h-10 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100" aria-labelledby="dropdown-button">
                             <option selected value="planno" className="inline-flex py-2 px-4 w-full hover:bg-gray-100">Plan ID</option>
                             <option value="name" >Name</option>
                             <option value="description" >Description</option>
                             <option value="validity" >Validity</option>
                         </select>
-                    <div id="dropdown" class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700" data-popper-reference-hidden="" data-popper-escaped="" data-popper-placement="top">
-                        
-                    </div>
                     <div class="relative w-full ">
                         <input type="search" value={searchText} onChange={handleSearch} id="search-dropdown" class="block  p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300"
-                         placeholder="Search for Mobile Plans by Id" />
+                         placeholder="Search for Mobile Plans by Id" autoComplete="off" >
+                         </input>
                             { isSearching && <button 
                             className="text-red-500 text-3xl absolute top-0 shadow-sm" 
                             style={{right: 50}}
                             onClick={() => {
                                 setSearchText('')
                                 setIsSearching(false)
+                                setShowAutoComp(false)
                             }}
                             >
                                 x
                             </button>}
+                            { autoSearchList.length>0 && <div className="auto-search-container">
+                                <ul>
+                                    {
+                                         autoSearchList.map((res) => {
+                                            return <li onClick={() => handleAutoSearch(res)} > {res} </li>
+                                        })
+                                    }
+                                </ul>
+                            </div>}
                             <button type="submit" onClick={search} className="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-[#336f7b] rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                 <svg aria-hidden="true" class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                                 <span class="sr-only">Search</span>
